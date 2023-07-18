352f6f502f6a932fdf57fd5748b749f44235079f
Oleksandr P <oleksandr.pasko.90@gmail.com>
2023-07-18 10:55:44 +0300

    Auto-generated commit [ci skip]

--- src/app/components/todo-item/todo-item.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { TodoItemComponent } from './todo-item.components';
-import { CommonModule } from '@angular/common';
-
-@NgModule({
-  declarations: [TodoItemComponent],
-  exports: [TodoItemComponent, CommonModule],
-})
-export class TodoItemModule {}
